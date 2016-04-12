d7fea3112a0bce8abeac8a2388468a2090bf4b8d
Erik van Egmond <erikvane@gmail.com>
2016-04-12 23:22:53 +0200

    Implemented the tree learner using sklearn

--- data_aggregator.py
+++ data_aggregator.py
@@ -1,14 +1,16 @@
 import pandas as pd
-from scipy.optimize import brute
-import statsmodels.api as sm
-from statsmodels.tsa.arima_model import ARIMA
+
 import numpy as np
 
-class DataAggregator:
-    def __init__(self, window_size=5):
-        self.window_size = window_size
 
-    def read(self, filepath):
+class DataAggregator:
+    def __init__(self, filepath, window_size=5):
+        self.window_size = window_size
+        dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
+        self.df = pd.read_csv(filepath, parse_dates=['time'], index_col='time', date_parser=dateparse)
+        print "data opened"
+
+    def read(self):
         """
         Generates the aggregated data. For each day there is the average mood (the value) and the features.
         There are a few possible variables. For each variable we take the sum and the average over each day. These
@@ -17,87 +19,80 @@ class DataAggregator:
         :return:
         """
 
-        dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
-        df = pd.read_csv(filepath, parse_dates=['time'], index_col='time', date_parser=dateparse)
-        print df.keys()
-        participants = df['id'].unique()  # all participants (don't know if needed)
-        vars = df['variable'].unique()  # all possible variables
 
+        participants = self.df['id'].unique()  # all participants (don't know if needed)
+        vars = self.df['variable'].unique()  # all possible variables
+
+        return self.window_combined_days(self.df, participants, vars, self.window_size)
+
+    def window_seperate_days(self, df, participants, vars, window_size):
         current_date = pd.datetime(2014, 4, 3)  # is set on midnigth, the start of the day.
-        start_date = current_date - pd.DateOffset(days=5)
+        start_date = current_date - pd.DateOffset(days=window_size)
         end_date = current_date
         print current_date, start_date, end_date
         # a query on the date range, for a specific pariticipant.
-        current_id = 'AS14.01'
-        mask = (df['id'] == current_id)
-        
-        # get every date for one person
-        tf = df.loc[mask]
-        tf = tf.ix[pd.to_datetime(tf.index).order()]
-        #print tf
-        #get the time range that we are interested in
-        time_range = tf[start_date:end_date]
-        #print time_range
+        for current_id in participants:
+            mask = (df['id'] == current_id)
 
-        # group by day
-        gs = time_range.groupby(pd.TimeGrouper(freq='D'))
-        for g in gs:
-            # g is a tuple, g[0] is the how the group is created, g[1] is the dataframe
-            gdf = g[1]
-            #print gdf
-            for var in vars:
-                varselection = gdf.loc[(gdf['variable'] == var)]
-                daymean = varselection['value'].mean()
-                daysum = varselection['value'].sum()
-                dayvar = varselection['value'].var() #variance
-                print var, daymean, daysum, dayvar
-            break
-        
-        
-        #print time_range.index
-        #return
-        a = np.unique(np.array(tf.index.map(pd.Timestamp.date)))
-        for i in range(len(a)):
-            if i > 5:
-                start = a[i - 5]
-                end = a[i]
-                five_days_vars = tf[start:end]
+            # get every date for one person
+            tf = df.loc[mask]
+            tf = tf.ix[pd.to_datetime(tf.index).sort_values()]
+
+            # get the time range that we are interested in
+            time_range = tf[start_date:end_date]
+            # print time_range
+
+            # group by day
+            groups = time_range.groupby(pd.TimeGrouper(freq='D'))
+            for group in groups:
+                # g is a tuple, g[0] is the how the group is created, g[1] is the dataframe
+                gdf = group[1]
                 for var in vars:
-                    #print var
-                    varselection = five_days_vars.loc[(five_days_vars['variable'] == var)]
-                    five_day_mean = varselection['value'].mean()
-                    five_day_sum = varselection['value'].sum()
-                    five_day_var = varselection['value'].var()
-                    if np.isnan(five_day_mean):
-                        print start, end, var, five_day_mean, five_day_sum, five_day_var
-                    else:
-                        print start, end, var, five_day_mean, five_day_sum, five_day_var
-                    #print five_day_mean, five_day_sum, five_day_var
-                    
-                    
-        #print ttf
-        
-        #df = df.set_index(['Timestamp'])     
-        #print onedate
-        # rolling window by 5 days
-        #fivedaymean = time_range.resample('5D').mean()
-        #maybe loop over the vars tomorrow morning
-        #for var in vars:
-        #    varselection = time_range.loc[(time_range['variable'] == var)]
-        #    print varselection
-            #a = varselection.rolling_window(time_range, window=5, win_type='parzen')
-            #print a
-        #fivedaysum = df.resample('D').sum().rolling(window=5).sum()
-        #fivedayvar = df.resample('D').var().rolling(window=5).var()
-        #print fivedaymean
-        
-    def objfunc(order, exog, endog):
-        fit = ARIMA(endog, order, exog).fit()
-        return fit.aic()
+                    varselection = gdf.loc[(gdf['variable'] == var)]
+                    daymean = varselection['value'].mean()
+                    daysum = varselection['value'].sum()
+                    dayvar = varselection['value'].var()  # variance
+                    print var, daymean, daysum, dayvar
+                break
 
+    def window_combined_days(self, df, participants, vars, window_size):
+        data = []
+        target = []
+        for current_id in participants:
+            mask = (df['id'] == current_id)
 
-        grid = (slice(1, 3, 1), slice(1, 3, 1), slice(1, 3, 1))
-        brute(objfunc, grid, args=(exog, endog), finish=None)      
-        
-        
-  #Use ARIMAResults.predict to cross-validate alternative models. The best approach would be to keep the tail of the time series (say most recent 5% of data) out of sample, and use these points to obtain the test error of the fitted models.      
+            # get every date for one person
+            tf = df.loc[mask]
+            tf = tf.ix[pd.to_datetime(tf.index).sort_values()]
+            a = np.unique(np.array(tf.index.map(pd.Timestamp.date)))
+            for i in range(len(a)):
+                if i > window_size:
+                    window_data = []
+                    start = a[i - window_size]
+                    end = a[i]
+                    five_days_vars = tf[start:end]
+                    for var in vars:
+                        varselection = five_days_vars.loc[(five_days_vars['variable'] == var)]
+                        five_day_mean = varselection['value'].mean()
+                        five_day_mean = five_day_mean if not np.isnan(five_day_mean) else 0
+                        five_day_sum = varselection['value'].sum()
+                        five_day_sum = five_day_sum if not np.isnan(five_day_sum) else 0
+                        five_day_var = varselection['value'].var()
+                        five_day_var = five_day_var if not np.isnan(five_day_var) else 0
+
+                        if np.isnan(five_day_mean):
+                            pass
+                            print current_id, start, end, var, five_day_mean, five_day_sum, five_day_var
+                        else:
+                            # print current_id, start, end, var, five_day_mean, five_day_sum, five_day_var
+                            window_data += [five_day_mean, five_day_sum, five_day_var]
+
+                    cur_day = tf[end:end + pd.DateOffset(days=1)]
+                    mood_selection = cur_day.loc[(cur_day['variable'] == 'mood')]
+                    if len(mood_selection) > 0:
+                        mood_mean = int(mood_selection['value'].mean())
+                        data.append(window_data)
+                        target.append(mood_mean)
+
+                    # target.append()
+        return np.array(data), np.array(target)
--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import data_aggregator as dr
+from scipy.optimize import brute
+from statsmodels.tsa.arima_model import ARIMA
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import cross_validation
+
+def test(window_size, data_aggregator):
+    data, target = data_aggregator.read()
+    print "\n\nTesting window size", window_size
+    for _ in range(1):
+        data_train, data_test, target_train, target_test = cross_validation.train_test_split(data, target, test_size=0.3)
+
+        clf = DecisionTreeClassifier()
+        clf = clf.fit(data_train, target_train)
+
+        data_score = clf.score(data_test, target_test)
+        print "score trained:", data_score
+
+    data_score = clf.score(data_test, [3] * len(target_test))
+    print "score 3:", data_score
+    data_score = clf.score(data_test, [4] * len(target_test))
+    print "score 4:", data_score
+    data_score = clf.score(data_test, [5] * len(target_test))
+    print "score 5:", data_score
+    data_score = clf.score(data_test, [6] * len(target_test))
+    print "score 6:", data_score
+    data_score = clf.score(data_test, [7] * len(target_test))
+    print "score 7:", data_score
+    data_score = clf.score(data_test, [8] * len(target_test))
+    print "score 8:", data_score
+
 
 filepath = 'data/dataset_mood_smartphone.csv'
 # filepath = 'data/dataset_small.csv'
 
-data_aggregator = dr.DataAggregator()
+data_aggregator = dr.DataAggregator(filepath)
+
+for window_size in range(1, 6):
+    test(window_size, data_aggregator)
 
 # Reading the data
-data_aggregator.read(filepath)
+
 
 
 """
@@ -17,3 +51,14 @@ Documentation on a arima implementation, should work with pandas stuff.
 http://chrisstrelioff.ws/sandbox/2015/06/08/decision_trees_in_python_with_scikit_learn_and_pandas.html
 a nice read about scikit, decision trees and pandas
 """
+
+
+def objfunc(order, exog, endog):
+    fit = ARIMA(endog, order, exog).fit()
+    return fit.aic()
+
+    grid = (slice(1, 3, 1), slice(1, 3, 1), slice(1, 3, 1))
+    brute(objfunc, grid, args=(exog, endog), finish=None)
+
+
+    # Use ARIMAResults.predict to cross-validate alternative models. The best approach would be to keep the tail of the time series (say most recent 5% of data) out of sample, and use these points to obtain the test error of the fitted models.
